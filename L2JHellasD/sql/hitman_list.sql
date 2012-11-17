c23ffd95f9e7fd3126cdeee5425abac0104c98bc
nightw0lf <nightw0lf>
2012-11-17 15:01:55 +0000

    Part 2/3 Deleting sql's from DataPack. Deleting some tool sql's for the next update.

--- L2JHellasD/sql/hitman_list.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `hitman_list` (
-  `targetId` int(16) NOT NULL default '0',
-  `clientId` int(16) NOT NULL default '0',
-  `target_name` varchar(24) NOT NULL default '',
-  `bounty` int(30) NOT NULL default '0',
-  `pending_delete` int(16) NOT NULL default '0',
-  PRIMARY KEY  (`targetId`)
-);
