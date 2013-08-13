a2df878de26d79153146be724dbb1bad813bd875
nightw0lf <nightw0lf>
2013-08-13 05:10:18 +0000

    Launchers upgrade

--- L2JHellasC/dist/GameServer_loop.sh
+++ L2JHellasC/dist/GameServer_loop.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
+
 # if any problems about bad intercepter a solution is
 # sudo apt-get install dos2unix
 # and in terminal convert all sh with: dos2unix *.sh
 
 err=1
-until [ $err == 0 ]; 
+until [ $err == 0 ];
 do
-#   . ./setenv.sh
-	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/java/`date +%Y-%m-%d_%H-%M-%S`_java0.log.0"
+	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/java/`date +%Y-%m-%d_%H-%M-%S`_java.log.0"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/stdout/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
-# For developers mostly (1. line gc logrotate, 2. line parameters for gc logging):
-#	[ -f log/gc.log ] && mv log/gc.log "log/gc/`date +%Y-%m-%d_%H-%M-%S`_gc.log"
-#	-verbose:gc -Xloggc:log/gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution
-# Alternative startup by NB4L1
-#	java -Dfile.encoding=UTF-8 -Xmn128m -Xms512m -Xmx1024m -server -cp ./../libs/*:l2jhellas.jar com.l2jhellas.gameserver.GameServer > log/stdout.log 2>&1
-	java -Dfile.encoding=UTF-8 -Xmx1024m -cp ./../libs/*:l2jhellas.jar com.l2jhellas.gameserver.GameServer > log/stdout.log 2>&1
+	
+	# For developers mostly (1. line gc logrotate, 2. line parameters for gc logging):
+	#[ -f log/gc.log ] && mv log/gc.log "log/gc/`date +%Y-%m-%d_%H-%M-%S`_gc.log" -verbose:gc -Xloggc:log/gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -XX:+PrintTenuringDistribution
+	java -Dfile.encoding=UTF-8 -Xmx768m -Xmn256m -Xmn256m -XX:PermSize=32m -cp ./../libs/*:l2jhellas.jar com.l2jhellas.gameserver.GameServer > log/stdout.log 2>&1
 	err=$?
-#	/etc/init.d/mysql restart
 	sleep 10;
 done
-
